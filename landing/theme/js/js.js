78dd93944d6dd9c3b6516b0c50fbba826f9d61e5
Musaev Ruslan <musya.wow@gmail.com>
2017-05-17 02:32:02 +0300

    slice

--- landing/theme/js/js.js
+++ landing/theme/js/js.js
@@ -1,26 +1,61 @@
 (function ($) {
 
-  if (typeof Drupal != 'undefined') {
-    Drupal.behaviors.wcportalMobileTheme = {
-      attach: function (context, settings) {
-        init();
-      },
-
-      completedCallback: function () {
-        // Do nothing. But it's here in case other modules/themes want to override it.
-      }
-    }
-  }
-
-  $(function () {
-    if (typeof Drupal == 'undefined') {
-      init();
-    }
+  $(document).ready(function () {
+    $('.nav a, .btn-next').bind("click", function (e) {
+      var anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+      }, 1000);
+      e.preventDefault();
+    });
+    return false;
   });
 
-  function init() {
+  $(window).load(function () {
+    $('.center-slider').slick({
+      centerMode: true,
+      autoplay: true,
+      infinite: true,
+      dots: true,
+      autoplaySpeed: 3000,
+      arrows:false,
+      variableWidth: true,
+      //centerPadding: '60px',
+      slidesToShow: 1
+    });
+  });
 
-  }
+  $(window).load(function () {
+    var $btn = $('.header-wrapper .btn-menu');
+
+    $btn.on('click touch', function (e) {
+      e.preventDefault();
+
+      $('body').toggleClass('mobile-active');
+    })
+  });
+
+  $(window).load(function () {
+    var $wrapper = $('.section-services'),
+      $nav = $wrapper.find('.tab-nav li'),
+      $content = $wrapper.find('.tab-content .items');
+
+    $nav.eq(0).addClass('active');
+    $content.eq(0).addClass('active');
+
+    $nav.on('click touch', function (e) {
+      e.preventDefault();
+
+      var $index = $(this).index();
+
+      if (!$(this).hasClass('active')) {
+        $nav.removeClass('active');
+        $(this).addClass('active');
+        $content.removeClass('active');
+        $content.eq($index).addClass('active');
+      }
+    })
+  })
 
 })(jQuery);
 
